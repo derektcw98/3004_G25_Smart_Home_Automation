8aef3df42189701d7a629bb724a44b66d085d210
eazi98 <xuezheng98@hotmail.com>
2022-07-01 15:29:44 +0800

    bias for data generation

--- generateData.py
+++ generateData.py
@@ -20,4 +20,38 @@ for monthDay in range(0,4):
         day = startDate + datetime.timedelta(days= day)
         day  = day.weekday()
         for x in timeProp(timeDate,143): #143 intervals of 10mins in a day
-            print(day, "\t" + str(x.strftime("%H:%M")), "\t" + str(round(random.uniform(26, 33),2)), "\t" + str(round(random.uniform(60,84),2)), "\t" + str(random.randint(0,1)), "\t" + str(random.randint(0,1)), "\t" + str(random.randint(23,29)), "\ttestRoom")
+            #bias for temperature and humidity based on time of day
+            if int(x.strftime("%H")) >= 10 and int(x.strftime("%H")) <= 19:
+                temp = round(random.uniform(30, 38),2)
+                humidity = round(random.uniform(60,80),2)
+            else:
+                temp = round(random.uniform(27, 34),2)
+                humidity = round(random.uniform(70,96),2)
+            #bias for temperature generation to determine aircon state
+            if temp > 25 and humidity > 70:
+                tempRand = random.randint(1,10)
+                if tempRand < 2:
+                    air_con = 0
+                else:
+                    air_con = 1
+            else:
+                tempRand = random.randint(1,10)
+                if tempRand < 2:
+                    air_con = 1
+                else:
+                    air_con = 0
+            #bias for time to determine light state
+            if int(x.strftime("%H")) >= 19 and int(x.strftime("%H")) <= 7:
+                lightRand = random.randint(1,10)
+                if lightRand < 2:
+                    light = 0
+                else:
+                    light = 1
+            else:
+                lightRand = random.randint(1,10)
+                if lightRand < 2:
+                    light = 1
+                else:
+                    light = 0
+            aircon_temp = random.randint(23,29)
+            print(day, "\t" + str(x.strftime("%H:%M")), "\t" + str(temp), "\t" + str(humidity), "\t" + str(light), "\t" + str(air_con), "\t" + str(aircon_temp), "\ttestRoom")
