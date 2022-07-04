6f29a0e1f7c7094cc4b2a2649edea866e5f567d7
twirle <acrylicblaze@gmail.com>
2022-07-05 03:30:52 +0800

    added sqlalchemy queries to retrieve csv of a month of data for modelling

--- db_insert_sqlalchemy.py
+++ db_insert_sqlalchemy.py
@@ -1,4 +1,4 @@
-from matplotlib.pyplot import connect
+import datetime
 import pandas as pd
 import sqlalchemy
 
@@ -21,13 +21,15 @@ def dropTable(engine):
 
 def insert(engine):
     # create table for sensor
-    engine.execute("create table if not exists sensors (sensor_id int primary key NOT NULL AUTO_INCREMENT, day int NOT NULL, hour int NOT NULL, minute int NOT NULL, temperature double NOT NULL, humidity double NOT NULL, light_state bit(1) NOT NULL, aircon_state bit(1) NOT NULL, aircon_temp int NOT NULL, room varchar(9) NOT NULL, class char(4) NOT NULL)")
+    # engine.execute("create table if not exists sensors (sensor_id int primary key NOT NULL AUTO_INCREMENT, day int NOT NULL, hour int NOT NULL, minute int NOT NULL, temperature double NOT NULL, humidity double NOT NULL, light_state bit(1) NOT NULL, aircon_state bit(1) NOT NULL, aircon_temp int NOT NULL, room varchar(9) NOT NULL, class char(4) NOT NULL)")
 
     # dataframe into mariadb
     with engine.begin() as conn:
-        df = pd.read_csv('data.csv')
+        df = pd.read_csv('models/data.csv')
         df.columns = ["day", "hour", "minute", "temperature", "humidity",
                       "light_state", "aircon_state", "aircon_temp", "room", "class"]
+        now = datetime.datetime.now()
+        df['datetime'] = now
         df.to_sql('sensors', conn, if_exists='append', index=False)
         print("Inserted")
 
@@ -38,18 +40,48 @@ def insert(engine):
 def queryRoom(engine, room):
     conn = engine.raw_connection()
     c = conn.cursor()
-    c.execute("SELECT * FROM sensors WHERE room = %s LIMIT 15", (room,))
-    # c.execute("SELECT * FROM sensors WHERE room = 'testRoom' LIMIT 15")
-    # query = "SELECT * FROM sensors WHERE room = 'testRoom' LIMIT 15"
-    # c.execute(query)
+    c.execute("SELECT * FROM sensors\
+        WHERE room = %s\
+        LIMIT 15", (room,))
     rows = c.fetchall()
     for row in rows:
         print(row)
 
 
+def retrieveMonth(engine, room):
+    conn = engine.raw_connection()
+    c = conn.cursor()
+    print(room)
+    # query = "SELECT * from sensors where room = %s AND date(datetime) between (curdate() - interval 1 month) and curdate() limit 15 INTO OUTFILE 'month.csv')"
+    # c.execute(query, (room,))
+    c.execute("""SELECT day, hour, minute, temperature, humidity, light_state, aircon_state, aircon_temp, room, class from sensors where room = %s AND date(datetime) between (curdate() - interval 1 month) and curdate() limit 15 INTO OUTFILE '../../var/lib/mysql/month.csv' FIELDS ENCLOSED BY '"' TERMINATED BY ',' ESCAPED BY '"' LINES TERMINATED BY '\n'""", (room,))
+    print("CSV written")
+
+
+def retrieveMonthPandas(engine, room):
+    conn = engine.raw_connection()
+    c = conn.cursor()
+    query = """SELECT day, hour, minute, temperature, humidity, light_state, aircon_state, aircon_temp, room, class from sensors where room = %(roomname)s AND date(datetime) between (curdate() - interval 1 month) and curdate() limit 15"""
+
+    df = pd.read_sql_query(sql=query, con=engine, params={"roomname": room})
+    df.to_csv('noclass.csv', index=False, header=None)
+    print("noclass.csv written")
+
+
+def trimCSV():
+    df = pd.read_csv('apps/mariadb/month.csv')
+    df = df.iloc[:, :-1]
+    df
+    df.to_csv('noclass.csv', index=False)
+    # df.columns = ["day", "hour", "minute", "temperature", "humidity",
+    #               "light_state", "aircon_state", "aircon_temp", "room", "class", "datetime"]
+    # df = df.drop(columns=['datetime'])
+    print("CSV trimmed")
+
+
 if __name__ == '__main__':
     engine = engine()
     insert(engine)
-    queryRoom(engine, input("Query room: "))
+    retrieveMonthPandas(engine, input("Query room: "))
 
 # weekly csv file using sensorlogger.py > save into containerised maria > query week/month's worth of data
