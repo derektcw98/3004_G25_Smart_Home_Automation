97e22f64e8d16585e2528130b21f8ee6e90fb8cc
twirle <acrylicblaze@gmail.com>
2022-07-05 03:34:04 +0800

    removed unused functions from sqlalchemy

--- db_insert_sqlalchemy.py
+++ db_insert_sqlalchemy.py
@@ -13,12 +13,6 @@ def engine():
     return engine
 
 
-def dropTable(engine):
-    conn = engine.raw_connection()
-    c = conn.cursor()
-    c.execute("DROP table sensors")
-
-
 def insert(engine):
     # create table for sensor
     # engine.execute("create table if not exists sensors (sensor_id int primary key NOT NULL AUTO_INCREMENT, day int NOT NULL, hour int NOT NULL, minute int NOT NULL, temperature double NOT NULL, humidity double NOT NULL, light_state bit(1) NOT NULL, aircon_state bit(1) NOT NULL, aircon_temp int NOT NULL, room varchar(9) NOT NULL, class char(4) NOT NULL)")
@@ -33,7 +27,7 @@ def insert(engine):
         df.to_sql('sensors', conn, if_exists='append', index=False)
         print("Inserted")
 
-    # my pc is getting 'mariadb.ProgrammingError: Cursor is closed', but it fine on my laptop
+    # my pc is getting 'mariadb.ProgrammingError: Cursor is closed', but it fine on my laptop/pi
     # issue could just be mariadb not being installed on my pc but doubt so cus i could still execute create table
 
 
@@ -48,16 +42,6 @@ def queryRoom(engine, room):
         print(row)
 
 
-def retrieveMonth(engine, room):
-    conn = engine.raw_connection()
-    c = conn.cursor()
-    print(room)
-    # query = "SELECT * from sensors where room = %s AND date(datetime) between (curdate() - interval 1 month) and curdate() limit 15 INTO OUTFILE 'month.csv')"
-    # c.execute(query, (room,))
-    c.execute("""SELECT day, hour, minute, temperature, humidity, light_state, aircon_state, aircon_temp, room, class from sensors where room = %s AND date(datetime) between (curdate() - interval 1 month) and curdate() limit 15 INTO OUTFILE '../../var/lib/mysql/month.csv' FIELDS ENCLOSED BY '"' TERMINATED BY ',' ESCAPED BY '"' LINES TERMINATED BY '\n'""", (room,))
-    print("CSV written")
-
-
 def retrieveMonthPandas(engine, room):
     conn = engine.raw_connection()
     c = conn.cursor()
@@ -66,17 +50,6 @@ def retrieveMonthPandas(engine, room):
     df = pd.read_sql_query(sql=query, con=engine, params={"roomname": room})
     df.to_csv('noclass.csv', index=False, header=None)
     print("noclass.csv written")
-
-
-def trimCSV():
-    df = pd.read_csv('apps/mariadb/month.csv')
-    df = df.iloc[:, :-1]
-    df
-    df.to_csv('noclass.csv', index=False)
-    # df.columns = ["day", "hour", "minute", "temperature", "humidity",
-    #               "light_state", "aircon_state", "aircon_temp", "room", "class", "datetime"]
-    # df = df.drop(columns=['datetime'])
-    print("CSV trimmed")
 
 
 if __name__ == '__main__':
