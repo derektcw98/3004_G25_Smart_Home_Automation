0880844bbabd48bc9b3eacdfa7a2a865a2879638
eazi98 <xuezheng98@hotmail.com>
2022-07-04 22:34:47 +0800

    binded models to docker-compose

---
+++ models/generateData.py
@@ -0,0 +1,107 @@
+import random
+import datetime
+import pandas
+
+#generate data based on common usual behaviours
+
+#day of week, hour, min, temperature, humidity, light on/off, aircon on/off, aircon temp, room, class
+
+# for step based float random generation
+def randfloat(start, stop, step):
+    return random.randint(0, int((stop - start) / step)) * step + start
+
+def timeProp(start,l):
+   current = start
+   curr = current
+   while l >= 0:
+      curr = curr + datetime.timedelta(minutes=10)
+      yield curr
+      l-=1
+
+startDate = datetime.datetime(2022, 7, 2, 0,00)
+timeDate = datetime.datetime(2022, 7, 1, 23,50)
+
+with open('dataTest.csv', 'w') as f:
+    for monthDay in range(0,4):
+        for day in range(0,7):
+            day = startDate + datetime.timedelta(days= day)
+            day  = day.weekday()
+            for x in timeProp(timeDate,143): #143 intervals of 10mins in a day
+
+                # bias for temperature and humidity based on time of day
+                if int(x.strftime("%H")) >= 7 and int(x.strftime("%H")) <= 19:      # 07 <-> 19 DAY TIME
+                    temp = randfloat(32, 36, 0.5)
+                    humidity = randfloat(60, 70, 0.5)
+                else:                                                               # 00 <-> 06 && 20 <-> 23 NIGHT TIME
+                    temp = randfloat(27, 31, 0.5)
+                    humidity = randfloat(71, 81, 0.5)
+
+                # bias for TEMPERATURE ONLY to determine aircon state
+                if temp >= 32:
+                    # bias air con ON 2:1(WD)/9:1(WE)
+                    if day <= 4:
+                        tempRand = random.randint(1,10)
+                    else:
+                        tempRand = random.randint(1,3)
+
+                    if tempRand == 1:
+                        air_con = 0
+                    else:
+                        air_con = 1
+                else:
+                    # bias air con OFF 9:1 
+                    tempRand = random.randint(1,10)
+                    if tempRand == 1:
+                        air_con = 1
+                    else:
+                        air_con = 0
+
+                # bias for time to determine light state
+                if int(x.strftime("%H")) >= 7 and int(x.strftime("%H")) <= 19:      # 07 <-> 19 DAY TIME
+                    # bias light OFF 9:1 
+                    lightRand = random.randint(1,10)
+                    if lightRand == 1:
+                        light = 1
+                    else:
+                        light = 0
+                else:                                                               # 00 <-> 06 && 20 <-> 23 NIGHT TIME
+                    # bias light ON 9:1
+                    lightRand = random.randint(1,10)
+                    if lightRand == 1:
+                        light = 0
+                    else:
+                        light = 1
+
+                # set random aircon temperature
+                aircon_temp = randfloat(23, 29, 0.5)
+
+                # #class generation
+                # if air_con == 0 and light == 0 and day < 5:
+                #     label = "wddc"
+                # elif air_con == 0 and light == 1 and day < 5:
+                #     label = "wdnc"
+                # elif air_con == 1 and light == 0 and day < 5: 
+                #     label = "wddh"
+                # elif air_con == 1 and light == 1 and day < 5:
+                #     label = "wdnh"
+                # elif air_con == 0 and light == 0 and day >= 5:
+                #     label = "wedc"
+                # elif air_con == 0 and light == 1 and day >= 5:
+                #     label = "wenc"
+                # elif air_con == 1 and light == 0 and day >= 5: 
+                #     label = "wedh"
+                # elif air_con == 1 and light == 1 and day >= 5:
+                #     label = "wenh"
+                #class generation (suppose to use data to produce result whether to turn on or off light/air_con)
+                if air_con == 0 and light == 0:
+                    label = "nanl"
+                elif air_con == 0 and light == 1:
+                    label = "nagl"
+                elif air_con == 1 and light == 0: 
+                    label = "ganl"
+                elif air_con == 1 and light == 1:
+                    label = "gagl"
+
+                print(day, "\t" + str(x.strftime("%H")),"\t" + str(x.strftime("%M")), "\t" + str(temp), "\t" + str(humidity), "\t" + str(light), "\t" + str(air_con), "\t" + str(aircon_temp), "\ttestRoom", "\t"+label)
+                data = str(day)+ "," + str(x.strftime("%H")) +"," + str(x.strftime("%M"))+ "," + str(temp) + "," + str(humidity) + "," + str(light) + "," + str(air_con) + "," + str(aircon_temp) +","+"testRoom" +","+ label + "\n"
+                f.write(data)
---
+++ models/generateModel.py
@@ -0,0 +1,84 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from joblib import dump
+
+
+#Read data from CSV generated from databbase here
+df = pd.read_csv('noclass.csv')
+
+labels = []
+
+#Create class based on device states
+for i in df.index:
+    lightState = int(df.iloc[i,5])
+    airconState = int(df.iloc[i,6])
+    if airconState == 0 and lightState == 0:
+        label = "nanl"
+    elif airconState == 0 and lightState == 1:
+        label = "nagl"
+    elif airconState == 1 and lightState == 0: 
+        label = "ganl"
+    elif airconState == 1 and lightState == 1:
+        label = "gagl"
+    labels.append(label)
+
+df.insert(9,"class", labels, True)
+
+#Add in column names
+#day of week, time, temperature, humidity, light on/off, aircon on/off, aircon temp, room, class
+df.columns = ["day", "hour", "minute", "temperature", "humidity", "light_state", "aircon_state", "aircon_temp", "room", "class"]
+
+
+# tentatively reduced to 5 features for POC
+y = df['class']
+
+#Reduce features
+X = df.drop('class', axis =1)
+X = X.drop('light_state', axis = 1)
+X = X.drop('aircon_state', axis = 1)
+X = X.drop('aircon_temp', axis = 1)
+X = X.drop('room', axis = 1)
+
+#Split into test and training set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=100)
+
+# Generate model for exporting
+n = 5
+knn = KNeighborsClassifier(n_neighbors=n)
+knn.fit(X_train, y_train)
+Y_predTrain = knn.predict(X_train)
+Y_predTest = knn.predict(X_test)
+targetAccuracy = accuracy_score(y_train, Y_predTrain)
+accuracy = accuracy_score(y_test, Y_predTest)
+print("Target Accuracy: " + str(round(targetAccuracy,4)),"Accuracy: " + str(round(accuracy,4)))
+
+#if accuracy less than 98% of training accuracyy, add neighbours
+while accuracy <= (0.98* targetAccuracy): #adjust multiplier accordingly
+    n += 2
+    classifier = KNeighborsClassifier(n_neighbors=n)
+    classifier.fit(X_train, y_train)
+    Y_predTrain = classifier.predict(X_train)
+    Y_predTest = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, Y_predTest)
+    print("Number of neighbours: " + str(n))
+    print("Target Accuracy: " + str(round(targetAccuracy,4)),"Accuracy: " + str(round(accuracy,4)), "Percent: " + str(round((accuracy/targetAccuracy),4)))
+else:
+    print("Target Accuracy Reached")
+
+#Create model to export
+knnPickle = open('knnPrediction', 'wb') 
+
+classifier = KNeighborsClassifier(n_neighbors=n)
+classifier.fit(X_train, y_train)
+
+# source, destination 
+dump(classifier, 'knnPrediction.joblib')  
+knnPickle.close()
+print("Model Saved")
+# #To predict on edge device
+# # load the model from disk
+# loaded_model = pickle.load(open('knnPrediction', 'rb'))
+# result = loaded_model.predict(X_test)
+# print(result)
